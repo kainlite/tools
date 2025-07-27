5ac0c2aa155c86487c0f80a8913deed2a8793fbc
kainlite <kainlite@gmail.com>
2025-07-27 20:26:18 -0300

    fix: linter

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+
+	}
 }
 
 func main() {
-  http.HandleFunc("/", handlerFunc)
-  fmt.Println("Starting the server on :3000...")
-  http.ListenAndServe(":3000", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err :=	w.Write([]byte("Hello, World!"))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+
+	}
+	})
+
+	mux.HandleFunc("/hello", handlerFunc)
+
+	srv := &http.Server{
+		Addr:         ":3000",
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Println("Server starting on :3000")
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
